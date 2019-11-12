b984fa9f721e424a0f3606d7d224dffa7c7672c8
codewarior123 <przemek.buszek@onet.eu>
2019-11-12 19:14:37 +0100

    id creator and random table

--- common.py
+++ common.py
@@ -4,6 +4,24 @@ implement commonly used functions here
 
 import random
 
+# FUNCTIONS TO CREATE ID
+# variables for generating random id
+numbers = '0123456789'
+letters = 'abcdefghijklmnoprstuwxyz'
+sings = "!#$%&'()*+,-./:_<=>?@][`^{|}~"
+id_index = 0
+
+
+# creates id
+def create_id():
+    elem_id1 = ''.join(random.sample(numbers, 2))
+    elem_id2 = ''.join(random.sample(letters, 2))
+    elem_id3 = ''.join(random.sample(sings, 2))
+    elem_id4 = ''.join(random.sample(letters, 2)).upper()
+    id = elem_id1 + elem_id2 + elem_id3 + elem_id4
+    return id
+
+
 
 def generate_random(table):
     """
@@ -19,7 +37,14 @@ def generate_random(table):
     """
 
     generated = ''
+    id_table = [line[id_index] for line in table]
+    generated = create_id()
+    if id in id_table:
+        generate_random(table)
+    else:
+        return generated
+
 
     # your code
 
-    return generated
+    
